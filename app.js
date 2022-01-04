11698c3a702c13de02300f4714a875ebe833c27c
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-01-04 02:57:49 -0800

    Added ability to add multiple projects

--- app.js
+++ app.js
@@ -1,14 +1,93 @@
 //require built in node file system module. Node api or modules include HTTP, URL, FS
 const inquirer = require('inquirer');
 
-inquirer.prompt([
-  {
-    type: 'input',
-    name: 'name',
-    message: 'What is your name?'
+/*function call for inquirer.prompt() The prompts are passed as object with a type (question type), name (property that identifies each question uniquely) and message (property that displays the question). So the questions will be stored in an array of objects where each object store the type, name and message of each question*/ 
+const promptUser = () =>  {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your Github Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide sopme additional information about yourself'
+    }
+  ]);
+ };
+ //we append the function to the .then()method since the function is a promise.
+ //promptUser().then(answers => console.log(answers));
+
+ /*function to capture user project data*/
+ const promptProject = portfolioData => {
+
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+  //if there is no projects array property, create one
+  if (!portfolioData.projects)  {
+    portfolioData.projects = [];
   }
-])
-.then(answers => console.log(answers));
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ])
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
+};
+ //we call the promptUser() promises using the .then() method to chain the questions running the promptUser questions before promptProject
+ promptUser()
+ .then(promptProject)
+ .then(portfolioData => {
+   console.log(portfolioData);
+ });
+  
+ 
 
 // const fs = require('fs');
 // const generatePage = require('./src/page-template.js');
