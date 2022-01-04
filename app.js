5da37c3e6e98c099d298a6cafd19df1c400e8d5a
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-01-04 01:44:10 -0800

    Added first question using inquirer

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 //require built in node file system module. Node api or modules include HTTP, URL, FS
-const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template.js');
 
-//declaring const to capture input excluding the node and app paths
-const profileDataArgs = process.argv.slice(2);
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  }
+])
+.then(answers => console.log(answers));
 
-/*Using assignment destructuring to store user input in command line captured fromm the process.argv array assigned to profileDataArgs. This is the same as declare the items individually i.e const name = profileDataArgs[0]; const github = profileDataArgs[1];*/
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-/*using fs.writeFile to create html document. fs.write file requires 3 arguments 1. the file name, 2 the data to write to the file (generatedPage() function) and a call back function that will be used for error handling*/
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// const pageHTML = generatePage(name, github);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// /*using fs.writeFile to create html document. fs.write file requires 3 arguments 1. the file name, 2 the data to write to the file (generatedPage() function) and a call back function that will be used for error handling*/
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw new Error(err);
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
