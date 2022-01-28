4031883c40d7d06885489a1703ced0937a6f83fe
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-01-27 17:31:51 -0800

    added validation to questions

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
 //require built in node file system module. Node api or modules include HTTP, URL, FS
 const inquirer = require('inquirer');
 
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
 /*function call for inquirer.prompt() The prompts are passed as object with a type (question type), name (property that identifies each question uniquely) and message (property that displays the question). So the questions will be stored in an array of objects where each object store the type, name and message of each question*/ 
 const promptUser = () =>  {
   return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name?',
+      /*adding input validation, requiring the user to input there name or they will get an error. Notice the validate method receives and argument nameInput*/
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your Github Username'
+      message: 'Enter your Github Username',
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your Github Username!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'about',
-      message: 'Provide sopme additional information about yourself'
+      message: 'Provide some additional information about yourself'
     }
   ]);
  };
- //we append the function to the .then()method since the function is a promise.
- //promptUser().then(answers => console.log(answers));
 
- /*function to capture user project data. We add the parameter portfolioData which will be used to store the project data*/
+ /*function to capture user project data. We add the parameter portfolioData which will be used to store each projects data*/
  const promptProject = portfolioData => {
 
   console.log(`
@@ -32,20 +50,36 @@ const promptUser = () =>  {
 Add a New Project
 =================
 `);
-  //if there is no projects array property, create an empty array portfolioData, the array will store all the project data
+  /*if there is no projects array property, create an empty array projects, the array will store all the project data. The if statement ensure that projects array is ony set to empty if there is no data in the array.*/
   if (!portfolioData.projects)  {
     portfolioData.projects = [];
   }
   return inquirer.prompt([
     {
       type: 'input',
-      name: 'name',
-      message: 'What is the name of your project?'
+      name: 'projectName',
+      message: 'What is the name of your project?',
+      validate: projectInput => {
+        if (projectInput) {
+          return true;
+        } else {
+          console.log('Please enter your project name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
+      validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log('Please enter a project description!');
+          return false;
+        }
+      }
     },
     {
       type: 'checkbox',
@@ -56,7 +90,15 @@ Add a New Project
     {
       type: 'input',
       name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: linkInput => {
+        if (linkInput) {
+          return true;
+        } else {
+          console.log('Please enter the GitHub link to your project!');
+          return false;
+        }
+      }
     },
     {
       type: 'confirm',
@@ -71,20 +113,20 @@ Add a New Project
       default: false
     }
   ])
-  /*once inquirer has collected the data we need to pass the data to the projects array using .then() the data is stored in projectData it is then pushed into the portfolioData project array*/
+  /*once inquirer has collected the data we need to pass the data to the projects array using .then() the data is stored in projectData it is then pushed into the portfolioData.project array established on line 38*/
   .then(projectData => {
     portfolioData.projects.push(projectData);
     /*this conditional statement checks if the user chose to add another project, by checking projectData to see if confirmAddProject evaluated to true. If confirmAddProject is true it will call promptProject and pass the existing portfolioData as a parameter so when it runs it doesnt reinitialize the project array to be blank at the conditoinal statement on line 36*/
     if (projectData.confirmAddProject) {
       return promptProject(portfolioData);
     } 
-    // if the user doesnt chose to add another project it will evaluate to false and return the  user data stored in portfolioData
+    // if the user doesnt chose to add another project it will evaluate to false and return the user data stored in portfolioData
     else {
       return portfolioData;
     }
   });
 };
- //we call the promptUser() promises using the .then() method to chain the questions running the promptUser questions before promptProject
+ /*we call the promptUser() function where the promise is appended in .then() which calls the promptProject funtion. It passes both the appended promptUser and promptProject pomises in the last .then() where the appended promises are passed into the portfolioData parameter and then the function console.logs the data*/
  promptUser()
  .then(promptProject)
  .then(portfolioData => {
@@ -93,8 +135,7 @@ Add a New Project
   
  
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+
 
 // const pageHTML = generatePage(name, github);
 
