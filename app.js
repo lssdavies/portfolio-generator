440dac25ccc6c5a37fd85afb7831aee5bea8af3e
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-01-27 23:58:07 -0800

    added additional html to the page-template and generated html using .filter and .map methods

--- app.js
+++ app.js
@@ -1,50 +1,51 @@
-//require built in node file system module. Node api or modules include HTTP, URL, FS
+/*require built in node file system module. Node api or modules include HTTP, URL, FS*/
+const fs = require('fs');
 const inquirer = require('inquirer');
+//assigns arrow function in page-template.js to generatePage()
+const generatePage = require('./src/page-template');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-/*function call for inquirer.prompt() The prompts are passed as object with a type (question type), name (property that identifies each question uniquely) and message (property that displays the question). So the questions will be stored in an array of objects where each object store the type, name and message of each question*/ 
-const promptUser = () =>  {
+/*function call for inquirer.prompt() The prompts are passed as object with a type (question type), name (property that identifies each question uniquely) and message (property that displays the question). So the questions will be stored in an array of objects where each object store the type, name and message of each question*/
+const promptUser = () => {
   return inquirer.prompt([
     {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+      type: "input",
+      name: "name",
+      message: "What is your name?",
       /*adding input validation, requiring the user to input there name or they will get an error. Notice the validate method receives and argument nameInput*/
-      validate: nameInput => {
+      validate: (nameInput) => {
         if (nameInput) {
           return true;
         } else {
-          console.log('Please enter your name!');
+          console.log("Please enter your name!");
           return false;
         }
-      }
+      },
     },
     {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your Github Username',
-      validate: githubInput => {
+      type: "input",
+      name: "github",
+      message: "Enter your Github Username",
+      validate: (githubInput) => {
         if (githubInput) {
           return true;
         } else {
-          console.log('Please enter your Github Username!');
+          console.log("Please enter your Github Username!");
           return false;
         }
-      }
+      },
     },
     // Confirm if user will like to add info about themselves
     {
-      type: 'confirm',
-      name: 'confirmAbout',
-      message: 'Would you like to enter some information about yourself for an "About" section?',
-      default: true
+      type: "confirm",
+      name: "confirmAbout",
+      message:
+        'Would you like to enter some information about yourself for an "About" section?',
+      default: true,
     },
     {
-      type: 'input',
-      name: 'about',
-      message: 'Provide some additional information about yourself',
+      type: "input",
+      name: "about",
+      message: "Provide some additional information about yourself",
       /*using when property to check if user will like to add or skip about question by looking at the response from confirmAbout*/
       when: ({ confirmAbout }) => {
         if (confirmAbout) {
@@ -52,111 +53,170 @@ const promptUser = () =>  {
         } else {
           return false;
         }
-      }
-    }
+      },
+    },
   ]);
- };
+};
 
- /*function to capture user project data and the data from promptUser is passed to the parameter portfolioData which will be used to store each user info and projects data*/
- const promptProject = portfolioData => {
-
+/*function to capture user project data and the data from promptUser is passed to the parameter portfolioData which will be used to store each user info and projects data*/
+const promptProject = (portfolioData) => {
   console.log(`
 =================
 Add a New Project
 =================
 `);
   /*if there the portfolioData object doesnt have a .projects array property, create an empty array projects within portfolioData, the array will store all the project data. The if statement ensure that portfolioData.projects array is ony set to empty if there is no data in the array.*/
-  if (!portfolioData.projects)  {
+  if (!portfolioData.projects) {
     portfolioData.projects = [];
   }
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'projectName',
-      message: 'What is the name of your project?',
-      validate: projectInput => {
-        if (projectInput) {
-          return true;
-        } else {
-          console.log('Please enter your project name!');
-          return false;
+  return (
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the name of your project?",
+          validate: (projectInput) => {
+            if (projectInput) {
+              return true;
+            } else {
+              console.log("Please enter your project name!");
+              return false;
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "description",
+          message: "Provide a description of the project (Required)",
+          validate: (descriptionInput) => {
+            if (descriptionInput) {
+              return true;
+            } else {
+              console.log("Please enter a project description!");
+              return false;
+            }
+          },
+        },
+        {
+          type: "checkbox",
+          name: "languages",
+          message:
+            "What did you build this project with? (Check all that apply)",
+          choices: [
+            "JavaScript",
+            "HTML",
+            "CSS",
+            "ES6",
+            "jQuery",
+            "Bootstrap",
+            "Node",
+          ],
+        },
+        {
+          type: "input",
+          name: "link",
+          message: "Enter the GitHub link to your project. (Required)",
+          validate: (linkInput) => {
+            if (linkInput) {
+              return true;
+            } else {
+              console.log("Please enter the GitHub link to your project!");
+              return false;
+            }
+          },
+        },
+        {
+          type: "confirm",
+          name: "feature",
+          message: "Would you like to feature this project?",
+          default: false,
+        },
+        {
+          type: "confirm",
+          name: "confirmAddProject",
+          message: "Would you like to enter another project?",
+          default: false,
+        },
+      ])
+      /*once inquirer has collected the data we need to pass the data to the portfolioData.projects array using .then() the promised data is is passed to the .then as the parameter projectData it is then pushed into the portfolioData.project array established on line 38*/
+      .then((projectData) => {
+        portfolioData.projects.push(projectData);
+        /*this conditional statement checks if the user chose to add another project, by checking projectData to see if confirmAddProject evaluated to true. If confirmAddProject is true it will call promptProject() and since projectData was pushed to the portfolioData.projects array in line 132 the array is not empty and it will not reinitialize the portfolioData.project array to be an empty array at the conditoinal statement on line 69. portfolioData is passed to promptProject() as a parameter so the data isnt lost*/
+        if (projectData.confirmAddProject) {
+          return promptProject(portfolioData);
         }
-      }
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Provide a description of the project (Required)',
-      validate: descriptionInput => {
-        if (descriptionInput) {
-          return true;
-        } else {
-          console.log('Please enter a project description!');
-          return false;
+        // if the user doesnt chose to add another project it will evaluate to false and return the user data stored in portfolioData
+        else {
+          return portfolioData;
         }
-      }
-    },
-    {
-      type: 'checkbox',
-      name: 'languages',
-      message: 'What did you build this project with? (Check all that apply)',
-      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-    },
-    {
-      type: 'input',
-      name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)',
-      validate: linkInput => {
-        if (linkInput) {
-          return true;
-        } else {
-          console.log('Please enter the GitHub link to your project!');
-          return false;
-        }
-      }
-    },
-    {
-      type: 'confirm',
-      name: 'feature',
-      message: 'Would you like to feature this project?',
-      default: false
-    },
-    {
-      type: 'confirm',
-      name: 'confirmAddProject',
-      message: 'Would you like to enter another project?',
-      default: false
-    }
-  ])
-  /*once inquirer has collected the data we need to pass the data to the portfolioData.projects array using .then() the promised data is is passed to the .then as the parameter projectData it is then pushed into the portfolioData.project array established on line 38*/
-  .then(projectData => {
-    portfolioData.projects.push(projectData);
-    /*this conditional statement checks if the user chose to add another project, by checking projectData to see if confirmAddProject evaluated to true. If confirmAddProject is true it will call promptProject() and since projectData was pushed to the portfolioData.projects array in line 132 the array is not empty and it will not reinitialize the portfolioData.project array to be an empty array at the conditoinal statement on line 69. portfolioData is passed to promptProject() as a parameter so the data isnt lost*/
-    if (projectData.confirmAddProject) {
-      return promptProject(portfolioData);
-    } 
-    // if the user doesnt chose to add another project it will evaluate to false and return the user data stored in portfolioData
-    else {
-      return portfolioData;
-    }
-  });
+      })
+  );
 };
- /*we call the promptUser() function and once that function is completed .then() calls the promptProject() and the promised data from prompt user is passed in as a parameter to portfolioData. promptProject once completed adds the data it captured to portfolioData as an array portfolioData.projects and in the last .then() takes portfolioData in as a parameter and then the function console.logs the data*/
- promptUser()
- .then(promptProject)
- .then(portfolioData => {
-   console.log(portfolioData);
- });
+/*we call the promptUser() function and once that function is completed .then() calls the promptProject() and the promised data from prompt user is passed in as a parameter to portfolioData. promptProject once completed adds the data it captured to portfolioData as an array portfolioData.projects and in the last .then() takes portfolioData in as a parameter and then the function console.logs the data*/
+
+promptUser()
+  .then(promptProject)
+  .then((portfolioData) => {
+    /* data in portfolioData object gets assigned to pageHTML as a parameter in generatePage()*/
+    const pageHTML = generatePage(portfolioData);
   
- 
+    /*using fs.writeFile to create html document. fs.write file requires 3 arguments 1. the file name and location, 2 the data to write to the file (generatedPage() function) and a call back function that will be used for error handling*/
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw new Error(err);
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+    console.log(portfolioData);
+  });
+
+// //using mockdata
+// const mockData = {
+//   name: 'Lernantino',
+//   github: 'lernantino',
+//   confirmAbout: true,
+//   about:
+//     'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+//   projects: [
+//     {
+//       name: 'Run Buddy',
+//       description:
+//         'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+//       languages: ['HTML', 'CSS'],
+//       link: 'https://github.com/lernantino/run-buddy',
+//       feature: true,
+//       confirmAddProject: true
+//     },
+//     {
+//       name: 'Taskinator',
+//       description:
+//         'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+//       languages: ['JavaScript', 'HTML', 'CSS'],
+//       link: 'https://github.com/lernantino/taskinator',
+//       feature: true,
+//       confirmAddProject: true
+//     },
+//     {
+//       name: 'Taskmaster Pro',
+//       description:
+//         'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+//       languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+//       link: 'https://github.com/lernantino/taskmaster-pro',
+//       feature: false,
+//       confirmAddProject: true
+//     },
+//     {
+//       name: 'Robot Gladiators',
+//       description:
+//         'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+//       languages: ['JavaScript'],
+//       link: 'https://github.com/lernantino/robot-gladiators',
+//       feature: false,
+//       confirmAddProject: false
+//     }
+//   ]
+// };
 
 
+// const pageHTML = generatePage(mockData);
 
-// const pageHTML = generatePage(name, github);
-
-// /*using fs.writeFile to create html document. fs.write file requires 3 arguments 1. the file name, 2 the data to write to the file (generatedPage() function) and a call back function that will be used for error handling*/
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw new Error(err);
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+// //end of mockData to be removed
--- src/page-template.js
+++ src/page-template.js
@@ -1,21 +1,175 @@
-/*arrow function to generate page. The function is accepting 2 arguments, it is structured to return a HTML as a string so the arrow needs to include the return statement and curly braces. This could be written in  asingle line/action where return would be implied and curly braces will not be needed. eg "const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;" if we weren't trying to create a HTML document. Template literals are used to pass ${name} and ${github} to the generated string(HTML)*/
-const generatePage = (name, github) => {
+// function to determin if about data is present to display on page
+/*create the about section the below checks to see if there is there isnt any data in the aboutText and if it is empy return nothing or esle there is data and to return the html for the about section*/
+const generateAbout = (aboutText) => {
+  if (!aboutText) {
+    return "";
+  }
+
   return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
+    <section class="my-3" id="about">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+      <p>${aboutText}</p>
+    </section>
+  `;
+  /* the function will be called using template literal in the location we want the about section to display and passing the data in about to it 
+  ${generateAbout(about)} line 58 below*/
+};
+
+/* this generateProjects function chains the filter() and map() methods to perform their functions within from within the template literals. this is achieved by applying .filter to the template literal feature as a param of the filter function based on the whether feature is true or false (feature or !feature). Then .map() is applied to the resulting and its value to genertate the html. This is cleaner and works better than the previous function which applied the .filter() and .map() methods to the html before placing is in the template. see commented out function below for refernece*/  
+const generateProjects = projectsArr => {
+  return `
+    <section class="my-3" id="portfolio">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+      <div class="flex-row justify-space-between">
+      ${projectsArr
+        .filter(({ feature }) => feature)
+        .map(({ name, description, languages, link }) => {
+          return `
+          <div class="col-12 mb-2 bg-dark text-light p-3">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+              Built With:
+              ${languages.join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+        `;
+        })
+        .join('')}
+
+      ${projectsArr
+        .filter(({ feature }) => !feature)
+        .map(({ name, description, languages, link }) => {
+          return `
+          <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+              Built With:
+              ${languages.join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+        `;
+        })
+        .join('')}
+      </div>
+    </section>
+  `;
+};
+
+/* This function was replaced by the above fuinction that chained the .filter() and .map() methods within the template literals was left in for reference. This function is for the projects section. It uses the .filter() method to create new arrays featuredProjects and nonFeaturedProjects based on the whether project.feature evaluates to true or false. we then create 2 more arrays for the html using the .map() on featuredProjectHtmlArr and nonFeaturedProjectHtmlArr the data in these arrays are interpolated into the returning object <section> element template literal. We use a join(', ') method to turn the featuredProjectHtmlArr and nonFeaturedProjectHtmlArr to a string separated by ', ' which was passed using join() */
+// const generateProjects = projectsArr => {
+//   // get array of just featured projects
+//   const featuredProjects = projectsArr.filter(project => {
+//     if (project.feature) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+
+//   // get array of all non-featured projects
+//   const nonFeaturedProjects = projectsArr.filter(project => {
+//     if (!project.feature) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+
+//   const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
+//     return `
+//       <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
+//         <h3 class="portfolio-item-title text-light">${name}</h3>
+//         <h5 class="portfolio-languages">
+//           Built With:
+//           ${languages.join(', ')}
+//         </h5>
+//         <p>${description}</p>
+//         <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+//       </div>
+//     `;
+//   });
+
+//   const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(
+//     ({ name, description, languages, link }) => {
+//       return `
+//         <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+//           <h3 class="portfolio-item-title text-light">${name}</h3>
+//           <h5 class="portfolio-languages">
+//             Built With:
+//             ${languages.join(', ')}
+//           </h5>
+//           <p>${description}</p>
+//           <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+//         </div>
+//       `;
+//     }
+//   );
+
+//   return `
+//     <section class="my-3" id="portfolio">
+//       <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+//       <div class="flex-row justify-space-between">
+//       ${featuredProjectHtmlArr.join('')}
+//       ${nonFeaturedProjectHtmlArr.join('')}
+//       </div>
+//     </section>
+//   `;
+// };
+
+
+/*templateData will accept the promise object returned by inquirer and the function has been updated to use promise object's properties*/
+module.exports = (templateData) => {
+
+
+  /*deconstructing object templateData by pulling the values we want by using the key name of the property we want to assign to a variable. this can be done any order with objects but we must ensure that wew're creating a variable name that completely matches the property name we're looking to deconstruct data from. If one uses the wrong property key name you wont get an error rather the variable will just be undefined. The below declaration reads create variable projects with all data from templateData. projects, create variable about with all the data data from templateData.about, and then create a object called hearder and assign all the remaining data to it*/
+  const { projects, about, ...header } = templateData;
+  /* [ ... ]  is the rest operator and is interchangeable with spread depending on use case*/
+
+  console.log(projects, about, header);
+  //data is passed from the above variables and accessed using template literals i.e. ${header.name} and ${header.github}
+
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portfolio Demo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
   </head>
 
   <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
+    <header>
+      <div class="container flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">${
+          header.name
+        }</h1>
+        <nav class="flex-row">
+          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
+            header.github
+          }">GitHub</a>
+        </nav>
+      </div>
+    </header>
+    <main class="container my-5">
+    ${generateAbout(about)}
+    ${generateProjects(projects)}
+    </main>
+    <footer class="container text-center py-3">
+      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${
+    header.name
+  }</h3>
+    </footer>
   </body>
   </html>
   `;
 };
-
-module.exports = generatePage;
+// notice the use of new Date().getFullYear() method to dynamically set the date
